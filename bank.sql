713fe06eec12317c78cbb0926822fb88ee64b3eb
eswaribala <Parameswaribala@gmail.com>
2025-07-21 14:22:39 +0530

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -1,4 +1,5 @@
 \c bankdb
 create type gendertype AS ENUM('MALE','FEMALE','OTHER');  
 create table IF NOT EXISTS customer(account_no bigint primary key, name text not null, address text, email text unique, contact_no bigint,password varchar(10),dob date,gender gendertype);
-create table IF NOT EXISTS account(account_no_fk bigint, runing_total bigint, opening_date date, constraint foreign key(account_no_fk) references customer(account_no) on delete set null on update cascade);
+create table IF NOT EXISTS account(account_no_fk bigint, runing_total bigint, opening_date date, constraint account_fk_constraint foreign key(account_no_fk) references customer(account_no) on delete set null on update cascade);
+create table IF NOT EXISTS transaction(account_no_fk bigint, amount bigint, time_stamp timestamp, sender text, receiver text, constraint transaction_fk_constraint foreign key(account_no_fk) references customer(account_no) on delete set null on update cascade);
