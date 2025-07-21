f3cdaed7da0cd9969c3933360f453e5727b04b12
eswaribala <Parameswaribala@gmail.com>
2025-07-21 15:10:55 +0530

    Create addaccount.sql

---
+++ addaccount.sql
@@ -0,0 +1,6 @@
+\c bankdb
+insert into account(account_no_fk,runing_total,opening_date) values (1001,500000,'2008-01-01');
+insert into account(account_no_fk,runing_total,opening_date) values (1002,560000,'1998-10-27');
+insert into account(account_no_fk,runing_total,opening_date) values (1003,10000,'2015-09-15');
+insert into account(account_no_fk,runing_total,opening_date) values (1004,20000,'2017-11-18');
+insert into account(account_no_fk,runing_total,opening_date) values (1005,26000,'2019-06-18');
