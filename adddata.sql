505c3c814ed169e6a62d5546df03d9ff73e1791f
eswaribala <Parameswaribala@gmail.com>
2025-07-21 14:56:20 +0530

    Create adddata.sql

---
+++ adddata.sql
@@ -0,0 +1,2 @@
+\c bankdb
+insert into customer(account_no,name,address,email,contact_no,password,dob,gender)values(1001,'parameswari','chennai','param@gmail.com',9951032861,'Test@123','1970-12-02','MALE');
