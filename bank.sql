c31a26a4e87964463196c97c287aef9d58619da0
eswaribala <Parameswaribala@gmail.com>
2025-07-21 12:14:44 +0530

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -1,4 +1,4 @@
 \c bankdb
 create type gendertype AS ENUM('MALE','FEMALE','OTHER');  
-create table IF NOT EXISTS customer(account_no bigint primary key, name text not null, address text, email text unique, contact_no bignint,password varchar(10),dob date,gender gendertype);
+create table IF NOT EXISTS customer(account_no bigint primary key, name text not null, address text, email text unique, contact_no bigint,password varchar(10),dob date,gender gendertype);
 
