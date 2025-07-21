ec92bac4f87d353a287db7e0543857ad106577d0
eswaribala <Parameswaribala@gmail.com>
2025-07-21 15:01:39 +0530

    Update adddata.sql

--- adddata.sql
+++ adddata.sql
@@ -1,2 +1,5 @@
 \c bankdb
-insert into customer(account_no,name,address,email,contact_no,password,dob,gender)values(1001,'parameswari','chennai','param@gmail.com',9951032861,'Test@123','1970-12-02','MALE');
+insert into customer(account_no,name,address,email,contact_no,password,dob,gender)values(1005,'eswari','chennai','eswari@gmail.com',9951032865,'Test@123','1970-12-02','FEMALE');
+insert into customer(account_no,name,address,email,contact_no,password,dob,gender)values(1002,'bala','bangalore','bala@gmail.com',9951032862,'Test@123','1965-04-27','MALE');
+insert into customer(account_no,name,address,email,contact_no,password,dob,gender)values(1003,'vignesh','pune','viki@gmail.com',9951032863,'Test@123','1995-12-07','MALE');
+insert into customer(account_no,name,address,email,contact_no,password,dob,gender)values(1004,'sanju','mumbai','sanju@gmail.com',9951032864,'Test@123','1998-12-12','FEMALE');
