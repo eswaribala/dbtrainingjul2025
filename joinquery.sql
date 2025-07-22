5e9561049556328da42b91f16945ebd36250a195
eswaribala <Parameswaribala@gmail.com>
2025-07-22 09:26:14 +0530

    Update joinquery.sql

--- joinquery.sql
+++ joinquery.sql
@@ -3,3 +3,4 @@ select  c.account_no,c.name,a.runing_total from customer c inner join account a 
 select  c.account_no,c.name,a.runing_total from customer c left join account a on c.account_no=a.account_no_fk; 
 select  c.account_no,c.name,a.runing_total from customer c right join account a on c.account_no=a.account_no_fk; 
 select  c.account_no,c.name,a.runing_total from customer c full outer join account a on c.account_no=a.account_no_fk; 
+select  sum(t.amount)  from customer c inner join transaction t on c.account_no=t.account_no_fk group by c.gender;
