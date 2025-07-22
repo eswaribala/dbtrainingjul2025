c0722917d524970db9d75ae661b5b48871118cf1
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-22 16:55:18 +0530

    updated

--- bankingapp/src/main/java/com/db/bankingapp/utils/TaxCalculator.java
+++ bankingapp/src/main/java/com/db/bankingapp/utils/TaxCalculator.java
@@ -10,13 +10,31 @@ public class TaxCalculator {
         long income=faker.number().numberBetween(3000000,5000000);
         Gender gender=getRandomGender();
         System.out.println(gender);
+        //income tax calculation
+        //if male and income > 30L, tax 30%
+        if((gender==Gender.MALE)&&(income>=3000000)){
+            System.out.println("Income Tax="+(income*0.3));
+        }
+        //if female and income > 30L, tax 20%
+        else if((gender==Gender.FEMALE)&&(income>=3000000)){
+            System.out.println("Income Tax="+(income*0.2));
+        }
+        //if other and income > 30L, tax 0%
+        else if((gender==Gender.OTHER)&&(income>=3000000)){
+            System.out.println("Income Tax="+(income*0.1));
+        }else
+           System.out.println("No Income Tax");
+
+
+
+
+
 
     }
 
     public static Gender getRandomGender(){
         Gender[]  values=Gender.values();
-        Random random=new Random();
-        return values[random.nextInt(values.length)];
+        return values[new Random().nextInt(values.length)];
 
     }
 }
