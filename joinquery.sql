9e40dca2d0638caca33aa72380438d56461faf33
eswaribala <Parameswaribala@gmail.com>
2025-07-22 11:04:11 +0530

    Update joinquery.sql

--- joinquery.sql
+++ joinquery.sql
@@ -8,5 +8,5 @@ select  sum(t.amount)  from customer c inner join transaction t on c.account_no=
 select  sum(t.amount)  from customer c inner join transaction t on c.account_no=t.account_no_fk group by c.address having c.address like 'pune';
 select max(runing_total) from account where runing_total <(select max(runing_total) from account);
 select average_amount from (select avg(runing_total) as average_amount from account) as stats;
-
+explain analyze select  c.account_no,c.name,a.runing_total from customer c inner join account a on c.account_no=a.account_no_fk; 
 create view customer_transaction as select  sum(t.amount)  from customer c inner join transaction t on c.account_no=t.account_no_fk group by c.gender;
