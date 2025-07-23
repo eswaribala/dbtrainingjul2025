1a2fbbd936d27dcd9a75cbe3a2c67d3b0d9f44ac
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-23 15:39:25 +0530

    updated

--- bankingapp/src/main/java/com/db/bankingapp/utils/FileIOApp.java
+++ bankingapp/src/main/java/com/db/bankingapp/utils/FileIOApp.java
@@ -2,6 +2,7 @@ package com.db.bankingapp.utils;
 
 import com.db.bankingapp.dao.FileHandler;
 import com.db.bankingapp.dao.FileHandlerImpl;
+import com.github.javafaker.Faker;
 
 import java.io.IOException;
 
@@ -9,6 +10,7 @@ public class FileIOApp {
     public static void main(String[] args) throws IOException {
 
         FileHandler fileHandler=new FileHandlerImpl();
-       System.out.println( fileHandler.createFile("logs","test.log"));
+        String fileName="test"+new Faker().number().numberBetween(1,10000)+".log";
+       System.out.println( fileHandler.createFile("logs",fileName));
     }
 }
