97c98cdbdea77ab23466db93c45b7bf3adaa462f
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-28 12:57:29 +0530

    updated

--- bankingapp/src/main/java/com/db/bankingapp/utils/DateTimeDemo.java
+++ bankingapp/src/main/java/com/db/bankingapp/utils/DateTimeDemo.java
@@ -1,9 +1,15 @@
 package com.db.bankingapp.utils;
 
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Set;
+import com.github.javafaker.Faker;
 
 public class DateTimeDemo {
     public static void main(String[] args) {
@@ -14,5 +20,14 @@ public class DateTimeDemo {
             //print time all over the world
             System.out.println(ZonedDateTime.now(ZoneId.of(zoneId)).format(formatter));
         }
+        LocalDate now = LocalDate.now();
+        LocalDate dob=new Faker().date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        System.out.println(dob);
+        // find out the age
+
+        System.out.println(ChronoUnit.YEARS.between(dob,now));
+
+
     }
 }
