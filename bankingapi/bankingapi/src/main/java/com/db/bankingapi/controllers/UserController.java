ab614c4365452f46a279bd57cb15a6394816b80e
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-29 16:10:43 +0530

    updated

---
+++ bankingapi/bankingapi/src/main/java/com/db/bankingapi/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.db.bankingapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    @Autowired
+    private RestClient restClient;
+
+    @GetMapping("/v1.0")
+    public ResponseEntity<String> getUsers(){
+        String response=restClient.get().uri("https://jsonplaceholder.typicode.com/users")
+                .retrieve()
+                .body(String.class);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+
+}
