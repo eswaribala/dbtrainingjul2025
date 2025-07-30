63fc32755721d6c61acfc3a7cad3bc06a74e1bfd
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-30 14:54:59 +0530

    updated

--- bankingapi/bankingapi/src/main/java/com/db/bankingapi/controllers/IndividualFormController.java
+++ bankingapi/bankingapi/src/main/java/com/db/bankingapi/controllers/IndividualFormController.java
@@ -1,6 +1,8 @@
 package com.db.bankingapi.controllers;
 
+import com.db.bankingapi.dto.Setting;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndividualFormController {
 
     @GetMapping("/v1.0")
-    public String loadIndexPage() {
-        return "index.html";
+    public String loadIndexPage(Model model) {
+        Setting setting=new Setting();
+        setting.setTitle("Individual Form");
+        model.addAttribute("setting",setting);
+        return "index";
     }
 }
