9f30b630d0347d401e94d650c42ad695a665b532
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-31 11:45:40 +0530

    updated

--- uiapplications/bankingapp/scripts/index.js
+++ uiapplications/bankingapp/scripts/index.js
@@ -1,4 +1,9 @@
 window.addEventListener('load',()=>{
-    alert('Welcome to the Banking App!');
+    //alert('Welcome to the Banking App!');
+    let options={month: 'long', day: 'numeric', year: 'numeric'};
+    document.querySelector('#article1 h2').textContent = 'News' +' | '+ new Date().toLocaleDateString('en-US', options);
 
-})
+     document.querySelector('#article2 h2').textContent = 'Media Release' +' | '+ new Date().toLocaleDateString('en-US', options);
+      document.querySelector('#article3 h2').textContent = 'Media Release' +' | '+ new Date().toLocaleDateString('en-US', options);
+     document.querySelector('#article4 h2').textContent = 'News' +' | '+ new Date().toLocaleDateString('en-US', options);
+    })
