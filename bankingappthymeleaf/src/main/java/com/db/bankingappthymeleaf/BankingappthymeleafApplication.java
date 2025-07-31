26f23b760b706f8d8bd067d14bace194ee9b6571
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-31 09:21:54 +0530

    updated

---
+++ bankingappthymeleaf/src/main/java/com/db/bankingappthymeleaf/BankingappthymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.db.bankingappthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingappthymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingappthymeleafApplication.class, args);
+    }
+
+}
---
+++ bankingappthymeleaf/src/test/java/com/db/bankingappthymeleaf/BankingappthymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.bankingappthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingappthymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
