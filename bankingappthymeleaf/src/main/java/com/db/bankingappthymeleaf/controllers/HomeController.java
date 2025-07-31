377f966c5b07e0b848adffca962c8d201ff255fd
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-07-31 09:24:27 +0530

    updated

---
+++ bankingappthymeleaf/src/main/java/com/db/bankingappthymeleaf/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.db.bankingappthymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String loadHomePage(){
+        return "index";
+    }
+
+
+}
