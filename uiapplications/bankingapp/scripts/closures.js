1a53c1f3e1cd9e99a601eeaff6928d0f08036776
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-08-01 13:02:55 +0530

    updated

--- uiapplications/bankingapp/scripts/closures.js
+++ uiapplications/bankingapp/scripts/closures.js
@@ -1,5 +1,6 @@
 function greet(){
     var name="Parameswari";
+    //private function
     function displayName(){
         return "Hello " + name;
     }
@@ -9,3 +10,18 @@ function greet(){
 var greetUser = greet();
 console.log(greetUser); // This will log "Hello Parameswari" to the console
 console.log(greetUser()); // This will call the displayName function and log "Hello Parameswari"
+
+
+var number = 10;
+function sum(){
+    function increment(){
+        number++;
+        return number;
+    }
+    return increment;
+}
+
+var test=sum();
+console.log(test()); // This will log 11
+console.log(test()); // This will log 11
+console.log(test()); // This will log 11
