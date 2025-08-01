22545a664ef15daa59233cebd3d6d48dc6720fed
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-08-01 14:26:40 +0530

    updated

--- uiapplications/bankingapp/scripts/exceptionsdemo.js
+++ uiapplications/bankingapp/scripts/exceptionsdemo.js
@@ -9,7 +9,18 @@ const AccountType = Object.freeze({
 
 function Account(id,runningTotal,roi,actType){
 
-    this.id = id;
+    let uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+   try{
+    if (!uuidPattern.test(id)) {
+        throw new Error("Invalid UUID format");
+    }else{
+        this.id = id;
+    }
+     }catch (error) {
+        console.error(error.message);
+     }
+
+    
     this.runningTotal = runningTotal;
     this.roi = roi;
     this.actType = actType;
@@ -19,7 +30,7 @@ const accountValue = Object.values(AccountType);
 const randomAccountType = accountValue[Math.floor(Math.random() * accountValue.length)];
 
 let account=new Account(
-    faker.string.uuid(),
+    faker.number.int({ min: 1000, max: 10000 }),
     faker.number.int({ min: 1000, max: 10000 }),
     faker.number.float({ min: 0.01, max: 0.1,
     precision: 0.01,
