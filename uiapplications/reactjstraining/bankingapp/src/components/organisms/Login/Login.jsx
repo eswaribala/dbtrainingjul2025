1ab8df94c65c3f4d1514cdd0dded380b67ef4d7b
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-08-04 10:32:29 +0530

    updated

--- uiapplications/reactjstraining/bankingapp/src/components/organisms/Login/Login.jsx
+++ uiapplications/reactjstraining/bankingapp/src/components/organisms/Login/Login.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 
-import styles from './Login.module.css';
 
-const Login = () => (
-  <div className={styles.Login} data-testid="Login">
-    Login Component
-  </div>
-);
+ import { useFormik } from 'formik';
+ 
+ const Login = () => {
+   // Note that we have to initialize ALL of fields with values. These
+   // could come from props, but since we don’t want to prefill this form,
+   // we just use an empty string. If we don’t do this, React will yell
+   // at us.
+   const formik = useFormik({
+     initialValues: {
+       userName: '',
+       password: '',
+     },
+     onSubmit: values => {
+       alert(JSON.stringify(values, null, 2));
+     },
+   });
+   return (
+     <form onSubmit={formik.handleSubmit}>
+       <label htmlFor="userName">User Name</label>
+       <input
+         id="userName"
+         name="userName"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.userName}
+       />
+ 
+        <label htmlFor="password">Password</label>
+       <input
+         id="password"
+         name="password"
+         type="password"
+         onChange={formik.handleChange}
+         value={formik.values.password}
+       />
 
-
+       <button type="submit">Submit</button>
+     </form>
+   );
+ };
 export default Login;
